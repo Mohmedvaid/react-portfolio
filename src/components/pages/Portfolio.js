9df4b45ea91307964b08a625009825c23030f8bd
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-07-05 14:09:48 -0500

    Added cards to portfolio page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -4,19 +4,110 @@ import myImage from '../assets/My-Img.jpeg'
 
 const Portfolio = () => (
   <div>
-    <h1>My Projects</h1>
-    <Card style={{ width: '18rem' }}>
+    <h1 className="heading">My Projects</h1>
+    <div className="card-container">
+    <Card className="card" style={{ width: '23rem', height:'30rem' }}>
       <Card.Img variant="top" src={myImage} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the bulk of
-          the card's content.
-        </Card.Text>
-        <Button variant="primary">Live</Button>
-        <Button variant="primary">Github</Button>
-      </Card.Body>
+          <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">The Shaker<i
+                class="material-icons right">more_vert</i></span>
+          </div>
+        <div class="card-reveal">
+          <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <div className="card-buttons">
+                <Button className="card-btn" variant="primary">Live</Button>
+                <Button className="card-btn" variant="primary">Github</Button>
+              </div>
+          </Card.Body>
+        </div>
     </Card> 
+    <Card className="card" style={{ width: '23rem', height:'30rem' }}>
+      <Card.Img variant="top" src={myImage} />
+          <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">The Shaker<i
+                class="material-icons right">more_vert</i></span>
+          </div>
+        <div class="card-reveal">
+          <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <div className="card-buttons">
+                <Button className="card-btn" variant="primary">Live</Button>
+                <Button className="card-btn" variant="primary">Github</Button>
+              </div>
+          </Card.Body>
+        </div>
+    </Card> 
+    <Card className="card" style={{ width: '23rem', height:'30rem' }}>
+      <Card.Img variant="top" src={myImage} />
+          <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">The Shaker<i
+                class="material-icons right">more_vert</i></span>
+          </div>
+        <div class="card-reveal">
+          <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <div className="card-buttons">
+                <Button className="card-btn" variant="primary">Live</Button>
+                <Button className="card-btn" variant="primary">Github</Button>
+              </div>
+          </Card.Body>
+        </div>
+    </Card> 
+    <Card className="card" style={{ width: '23rem', height:'30rem' }}>
+      <Card.Img variant="top" src={myImage} />
+          <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">The Shaker<i
+                class="material-icons right">more_vert</i></span>
+          </div>
+        <div class="card-reveal">
+          <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <div className="card-buttons">
+                <Button className="card-btn" variant="primary">Live</Button>
+                <Button className="card-btn" variant="primary">Github</Button>
+              </div>
+          </Card.Body>
+        </div>
+    </Card> 
+
+    <Card className="card" style={{ width: '23rem', height:'30rem' }}>
+      <Card.Img variant="top" src={myImage} />
+          <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">The Shaker<i
+                class="material-icons right">more_vert</i></span>
+          </div>
+        <div class="card-reveal">
+          <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <div className="card-buttons">
+                <Button className="card-btn" variant="primary">Live</Button>
+                <Button className="card-btn" variant="primary">Github</Button>
+              </div>
+          </Card.Body>
+        </div>
+    </Card> 
+    </div>
   </div>
 );
 
