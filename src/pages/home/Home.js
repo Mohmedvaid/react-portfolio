ab8c61047727dc0ac3d48ed04da62a87f74d8ee2
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-08-22 19:37:47 -0500

    updated images on homepage, removed video

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Card, Button } from 'react-bootstrap';
+// import { Card, Button } from 'react-bootstrap';
+import { Card, Col, Row, Icon, CardTitle } from 'react-materialize'
 import './styles.css'
 
 const cardSize = {
@@ -14,21 +15,21 @@ const Home = (props) => (
         <div className="parallax"></div>
         <div className="centered">Hello! My name is Mohmed Vaid.</div>
 
-        <div  className="vid-img-container">
-        <div   className="video">
-            <video  autoPlay muted loop style={{width: "100%", height: "100%"}} className="video-main"> 
-            <source src="/assets/Portfolio-Video.mp4" type="video/mp4"/>
-            </video>
-        </div>
-        
+        <div  className="vid-img-container">        
         <div className ="myImg-container">
-          <img className="about-img"src="/assets/My-Img.jpeg" alt="Mohmed vaid Profile " />
+          <img className="about-img"src="/assets/my-img-code-bg-final.png" alt="Mohmed vaid Profile " />
           <div className="overlay">
             <div className="text">
            <p> <div className="text-code">&lt;head&gt;</div>coder: 50%;<br/> entrepreneur: 50%;<br/> <div className="text-code"> &lt;/head&gt;</div></p> 
            <p><div className="text-code">&lt;body&gt;<br/></div> baseball: 75%;<br/> gym: 25%;<br/> <div className="text-code"> &lt;/body&gt;</div></p>
             </div>
           </div>
+        </div>
+        <div   className="video">
+           <img className="tech-img Es6-nodeJS" src="/assets/tech-images/Es6-nodeJS.jpg"></img>
+            <img className="tech-img html-css" src="/assets/tech-images/html-css.jpg"></img>
+            <img className="tech-img Es6-nodeJS" src="/assets/tech-images/MERN.jpg"></img>
+
         </div>
 
         </div>
@@ -39,44 +40,46 @@ const Home = (props) => (
         <div className="aboutme">
             <p className="heading">Recent Work</p>
             <div className="recentwork-container">
-                <Card className="card" style={cardSize}>
-                  <Card.Img variant="top" src="/assets/project-images/Password-Generator-Img.png" />
-                      <div className="card-content">
-                        <span className="card-title activator grey-text text-darken-4">The Shaker<i
-                            className="material-icons right">more_vert</i></span>
-                      </div>
-                    <div className="card-reveal">
-                      <Card.Body>
-                          <Card.Title>Password Generator <i className="material-icons right">close</i></Card.Title>
-                          <Card.Text>
-                            A simple app that generates passwords for user according to the specifications
-                          </Card.Text>
-                          <div className="card-buttons">
-                            <Button href="https://mohmedvaid.github.io/homework3/Develop/" className="card-btn" variant="primary">Live</Button>
-                            <Button href="https://github.com/Mohmedvaid/homework3/tree/master/Develop" className="card-btn" variant="primary">Github</Button>
-                          </div>
-                      </Card.Body>
-                    </div>
-                </Card> 
-                <Card className="card" style={cardSize}>
-                  <Card.Img variant="top" src="/assets/project-images/Password-Generator-Img.png" />
-                      <div className="card-content">
-                        <span className="card-title activator grey-text text-darken-4">The Shaker<i
-                            className="material-icons right">more_vert</i></span>
-                      </div>
-                    <div className="card-reveal">
-                      <Card.Body>
-                          <Card.Title>Password Generator <i className="material-icons right">close</i></Card.Title>
-                          <Card.Text>
-                            A simple app that generates passwords for user according to the specifications
-                          </Card.Text>
-                          <div className="card-buttons">
-                            <Button href="https://mohmedvaid.github.io/homework3/Develop/" className="card-btn" variant="primary">Live</Button>
-                            <Button href="https://github.com/Mohmedvaid/homework3/tree/master/Develop" className="card-btn" variant="primary">Github</Button>
-                          </div>
-                      </Card.Body>
-                    </div>
-                </Card> 
+                <Row>
+                  <Col
+                    m={6}
+                    s={12}
+                  >
+                    <Card
+                      closeIcon={<Icon>close</Icon>}
+                      header={<CardTitle image="/assets/project-images/Password-Generator-Img.png" reveal waves="light"/>}
+                      reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}
+                      revealIcon={<Icon>more_vert</Icon>}
+                      title="Card Title"
+                    >
+                      <p>
+                        <a href="#">
+                          This is a link
+                        </a>
+                      </p>
+                    </Card>
+                  </Col>
+                </Row> 
+                <Row>
+                  <Col
+                    m={6}
+                    s={12}
+                  >
+                    <Card
+                      closeIcon={<Icon>close</Icon>}
+                      header={<CardTitle image="/assets/project-images/Password-Generator-Img.png" reveal waves="light"/>}
+                      reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}
+                      revealIcon={<Icon>more_vert</Icon>}
+                      title="Card Title"
+                    >
+                      <p>
+                        <a href="#">
+                          This is a link
+                        </a>
+                      </p>
+                    </Card>
+                  </Col>
+                </Row> 
                 
 
             </div>
