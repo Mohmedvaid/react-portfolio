3d1d4d268b35bd7b420bdb3b3a781749fb3e2d12
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-08-23 18:57:23 -0500

    added styles to home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -15,25 +15,26 @@ const Home = (props) => (
         <div className="parallax"></div>
         <div className="centered">Hello! I'm <span className="dark-pink">Mohmed Vaid</span>. I'm full-stackweb developer.</div>
 
-        <div  className="vid-img-container">        
-        <div className ="myImg-container">
-          <img className="about-img"src="/assets/my-img-code-bg-final.png" alt="Mohmed vaid Profile " />
-          <div className="overlay">
-            <div className="text">
-            <div className="text-code">&lt;mohmed&gt;<br/></div>
-            <blockquote>
-            <p><div className="text-code">&lt;head&gt;</div><blockquote>getBrain = () => &#123;<br/> <blockquote>return &#123;<br/>software engineer: 50%;<br/>entrepreneur: 50%;<br/>&#125;</blockquote>&#125;;</blockquote> <div className="text-code"> &lt;/head&gt;</div></p> 
-            <p className="code-body"><div className="text-code">&lt;body&gt;<br/></div><blockquote>&lt;baseball&gt;75%&lt;/baseball&gt;<br/>&lt;gym&gt;25%&lt;/gym&gt;</blockquote><div className="text-code"> &lt;/body&gt;</div></p>
-            </blockquote>
-            <div className="text-code">&lt;/mohmed&gt;<br/></div>
+        <div  className="vid-img-container"> 
+          <div className ="myImg-container">
+            <p className="hover-text-lg"> Hover over the image for more info!</p>
+            <p className="hover-text-s"> Tap on the image for more info!</p>
+            <img className="about-img"src="/assets/my-img-code-bg-final.png" alt="Mohmed vaid profile" />
+            <div className="overlay">
+              <div className="text">
+              <div className="text-code">&lt;mohmed&gt;<br/></div>
+              <blockquote>
+              <p><div className="text-code">&lt;head&gt;</div><blockquote>getBrain = () => &#123;<br/> <blockquote>return &#123;<br/>software engineer: 50%;<br/>entrepreneur: 50%;<br/>&#125;</blockquote>&#125;;</blockquote> <div className="text-code"> &lt;/head&gt;</div></p> 
+              <p className="code-body"><div className="text-code">&lt;body&gt;<br/></div><blockquote>&lt;baseball&gt;75%&lt;/baseball&gt;<br/>&lt;gym&gt;25%&lt;/gym&gt;</blockquote><div className="text-code"> &lt;/body&gt;</div></p>
+              </blockquote>
+              <div className="text-code">&lt;/mohmed&gt;<br/></div>
+              </div>
             </div>
           </div>
-        </div>
         <div className="video">
            <img className="tech-img Es6-nodeJS" src="/assets/tech-images/es6-nodeJS.jpg"></img>
             <img className="tech-img html-css" src="/assets/tech-images/html-css.jpg"></img>
             <img className="tech-img Es6-nodeJS" src="/assets/tech-images/MERN.jpg"></img>
-
         </div>
 
         </div>
@@ -41,9 +42,9 @@ const Home = (props) => (
         <div className="aboutme aboutme-main">
             <p className="heading">About Me</p>
             <p className ="aboutpara">
-            I am a Full Stack Web Developer based in Chicago IL, leveraging my background in computer science to build efficient and powerful web applications. I recently earned a certificate in Full Stack Web Development from Northwestern University, where I developed skills in JavaScript, React.js, Node.js, Axios, Express, MongoDB, SQL, JavaScript, CSS, HTML, and Python to develop a powerful full stack and server-side applications. Known as an innovative problem-solver passionate about developing meaningful apps, with a focus on time and space complexity algorithms and mobile-first design development. <br/>
+            I am a Full Stack Web Developer based in Chicago IL, leveraging my background in computer science to build efficient and powerful web applications. I recently earned a certificate in Full Stack Web Development from Northwestern University, where I developed skills in JavaScript, React.js, Node.js, Axios, Express, MongoDB, SQL, JavaScript, CSS, HTML, and Python to develop a powerful full stack and server-side applications.<br/><br/> Known as an innovative problem-solver passionate about developing meaningful apps,with a focus on time and space complexity algorithms and mobile-first design development. Willingness to learn about entrepreneurship and business challenges from a diverse perspective and collaborating with others to create meaningful solutions. 
             
-            Willingness to learn about entrepreneurship and business challenges from a diverse perspective and collaborating with others to create meaningful solutions. With each project, I aim to best engage my audience for impactful user experience. I applied aspects of UX, computer science fundamentals, and agile development in a recent project where I worked in a team of four to develop a MERN app that helps dog owners to find a buddy for their pets.  Fascinated by computer science fundamentals and breaking down a problem piece-by-piece to find a solution. I’m excited to leverage my skills as part of a fast-paced, quality-driven team to build better experiences on the web.
+            {/* Willingness to learn about entrepreneurship and business challenges from a diverse perspective and collaborating with others to create meaningful solutions. With each project, I aim to best engage my audience for impactful user experience. I applied aspects of UX, computer science fundamentals, and agile development in a recent project where I worked in a team of four to develop a MERN app that helps dog owners to find a buddy for their pets.  Fascinated by computer science fundamentals and breaking down a problem piece-by-piece to find a solution. I’m excited to leverage my skills as part of a fast-paced, quality-driven team to build better experiences on the web. */}
               </p>
         </div>
         </div>
