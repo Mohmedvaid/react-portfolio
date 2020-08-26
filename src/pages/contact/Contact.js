be4c5894478c8db00217d0dbd5dec705a8025c96
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-08-25 23:05:59 -0500

    updated resume

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./styles.css"
 
 const Contact = () => (
   <div>
@@ -7,20 +8,20 @@ const Contact = () => (
 
       <div className="phone">
           <p className="form-label">Phone</p>
-          <p className="form-data">+1(312)975 2811</p>
+          <p className="form-data monospaceFont">+1(312)975 2811</p>
       </div>
 
       <div className="email">
           <p className="form-label">Email</p>
-          <p className="form-data">Mohmedvaid@gmail.com</p>
+          <p className="form-data monospaceFont">Mohmedvaid@gmail.com</p>
       </div>
       
       <div className="social-media">
           <p className="form-label">Social Media</p>
           <div className="social-icons">
-              <a style={{color:"lightslategrey"}} href="/public/assets/My-resume-v1.0.pdf"><i className="far fa-file fa-5x resume-icon" aria-hidden="true"></i></a>
-              <a style={{color:"lightslategrey"}} href="https://www.linkedin.com/in/mohmedhusain-v-0374b2128/"><i className="fab fa-linkedin fa-5x linkedin-icon" aria-hidden="true"></i></a>
-              <a style={{color:"lightslategrey"}} href="https://github.com/Mohmedvaid/"><i className="fab fa-github-square fa-5x github-icon" aria-hidden="true"></i></a>
+              <a style={{color:"lightslategrey"}} href="/public/assets/Mohmedhusain_Vaid_Resume.docx"><i className="far fa-file fa-4x resume-icon" aria-hidden="true"></i></a>
+              <a style={{color:"lightslategrey"}} href="https://www.linkedin.com/in/mohmedhusain-v-0374b2128/"><i className="fab fa-linkedin fa-4x linkedin-icon" aria-hidden="true"></i></a>
+              <a style={{color:"lightslategrey"}} href="https://github.com/Mohmedvaid/"><i className="fab fa-github-square fa-4x github-icon" aria-hidden="true"></i></a>
           </div>
       </div>
     </div>
