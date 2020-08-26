08982d420f0f3e9a478d19775f5c57a39b74155d
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-08-25 22:30:00 -0500

    added project descriptions

--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -17,12 +17,22 @@ const Portfolio = () => (
         className="portfolio-cards"
           closeIcon={<Icon>close</Icon>}
           header={<CardTitle image="/assets/project-images/fetch.png" reveal waves="light"/>}
-          reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}
+          reveal={<><p>An app that helps pet owner find new friends for their furry family members. Created using MERN stack, React-image-crop Mongoose, Axios, Materialize, Multer, Imgur, Animate.css, Passport
+            Raise Da Woof!<br/><br/>
+            </p>
+            <div className="cardLinks">
+            <a href="https://fetch-woof-312.herokuapp.com/" target="_blank">
+                Deployed Site
+              </a><br/>
+              <a href="https://github.com/raise-da-woof/fetch" target="_blank">
+                Github
+              </a>
+            </div></>}
           revealIcon={<Icon>more_vert</Icon>}
           title="Fetch"
         >
           <p >
-            <a href="https://github.com/raise-da-woof/fetch">
+            <a href="https://github.com/raise-da-woof/fetch" target="_blank">
               Github
             </a>
           </p>
@@ -39,17 +49,22 @@ const Portfolio = () => (
           className="portfolio-cards"
             closeIcon={<Icon>close</Icon>}
             header={<CardTitle image="/assets/project-images/mypantry.png" reveal waves="light"/>}
-            reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}
-            revealIcon={<Icon>more_vert</Icon>}
+            reveal={<><p>App that provides recipes to users with ingredients the users have on hand. Built with Spoonacular API, MySQL, Passport, Bulma,Express, Express Handlebars, ESLint. Travis.<br/><br/>
+              </p>
+              <div className="cardLinks">
+              <a href="https://shielded-harbor-38774.herokuapp.com/" target="_blank">
+                  Deployed Site
+                </a><br/>
+                <a href="https://github.com/Hot-Fixers/mypantry" target="_blank">
+                  Github
+                </a>
+              </div></>}            revealIcon={<Icon>more_vert</Icon>}
             title="My Pantry"
           >
             <p className="project-links">
-              <a href="https://github.com/Hot-Fixers/mypantry">
+              <a href="https://github.com/Hot-Fixers/mypantry" target="_blank">
                 Github
               </a>
-              {/* <a href="https://shielded-harbor-38774.herokuapp.com/">
-                Live Site
-              </a> */}
             </p>
           </Card>
         </Col>
@@ -64,12 +79,20 @@ const Portfolio = () => (
         className="portfolio-cards"
           closeIcon={<Icon>close</Icon>}
           header={<CardTitle image="/assets/project-images/shaker.png" reveal waves="light"/>}
-          reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}
-          revealIcon={<Icon>more_vert</Icon>}
+          reveal={<><p>Shaker is a web app for discovering new cocktails and drinks. Favorite drinks can be stored in localStorage, recalled for future use and deleted is necessary. Shaker also features the Google maps api, allow users to pull up a map of bars near them that may be serving the new and interesting cocktails they find!<br/><br/>
+            </p>
+            <div className="cardLinks">
+            <a href="https://json-d3rulo.github.io/shaker/" target="_blank">
+                Deployed Site
+              </a><br/>
+              <a href="https://github.com/JSON-D3RULO/shaker" target="_blank">
+                Github
+              </a>
+            </div></>}          revealIcon={<Icon>more_vert</Icon>}
           title="Shaker"
         >
           <p >
-            <a href="https://github.com/JSON-D3RULO/shaker">
+            <a href="https://github.com/JSON-D3RULO/shaker" target="_blank">
               Github
             </a>
           </p>
@@ -85,12 +108,22 @@ const Portfolio = () => (
         className="portfolio-cards"
           closeIcon={<Icon>close</Icon>}
           header={<CardTitle image="/assets/project-images/fitness-tracker.png" reveal waves="light"/>}
-          reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}
-          revealIcon={<Icon>more_vert</Icon>}
+          reveal={<><p>The fitness tracker allows users to create, track and manage workouts/exercises using Chart.js. Its built using Express, MongoDB, Mongoose, and Chart js.
+
+            <br/><br/>
+            </p>
+            <div className="cardLinks">
+            <a href="https://full-fitness.herokuapp.com/" target="_blank">
+                Deployed Site
+              </a><br/>
+              <a href="https://github.com/Mohmedvaid/fitness-tracker" target="_blank">
+                Github
+              </a>
+            </div></>}          revealIcon={<Icon>more_vert</Icon>}
           title="Fitness Tracker"
         >
           <p >
-            <a href="https://github.com/Mohmedvaid/fitness-tracker">
+            <a href="https://github.com/Mohmedvaid/fitness-tracker" target="_blank">
               Github
             </a>
           </p>
@@ -106,12 +139,20 @@ const Portfolio = () => (
         className="portfolio-cards"
           closeIcon={<Icon>close</Icon>}
           header={<CardTitle image="/assets/project-images/cr-shift.png" reveal waves="light"/>}
-          reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}
-          revealIcon={<Icon>more_vert</Icon>}
+          reveal={<><p>A simple app that allows the users to add names and schedules times for any activity. The app then automatically creates displays shift according to the availibility of the people. Its created with jQuery, MongoDB and Express<br/><br/>
+            </p>
+            <div className="cardLinks">
+            <a href="https://cr-shift.herokuapp.com/" target="_blank">
+                Deployed Site
+              </a><br/>
+              <a href="https://github.com/Mohmedvaid/cr-shift" target="_blank">
+                Github
+              </a>
+            </div></>}          revealIcon={<Icon>more_vert</Icon>}
           title="CR-Shift Scheduler"
         >
           <p >
-            <a href="https://github.com/Mohmedvaid/cr-shift">
+            <a href="https://github.com/Mohmedvaid/cr-shift" target="_blank">
               Github
             </a>
           </p>
@@ -127,12 +168,20 @@ const Portfolio = () => (
         className="portfolio-cards"
           closeIcon={<Icon>close</Icon>}
           header={<CardTitle image="/assets/project-images/note-taker.png" reveal waves="light"/>}
-          reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}
-          revealIcon={<Icon>more_vert</Icon>}
-          title="CR-Shift Scheduler"
+          reveal={<><p>Note takers is simple app that allows the users to create and save notes for future. The notes are saved in the local storage of the browser<br/><br/>
+            </p>
+            <div className="cardLinks">
+            <a href="https://whispering-sea-22703.herokuapp.com/" target="_blank">
+                Deployed Site
+              </a><br/>
+              <a href="https://github.com/Mohmedvaid/note-taker" target="_blank">
+                Github
+              </a>
+            </div></>}          revealIcon={<Icon>more_vert</Icon>}
+          title="Note Taker"
         >
           <p >
-            <a href="https://github.com/Mohmedvaid/note-taker">
+            <a href="https://github.com/Mohmedvaid/note-taker" target="_blank">
               Github
             </a>
           </p>
@@ -148,12 +197,18 @@ const Portfolio = () => (
         className="portfolio-cards"
           closeIcon={<Icon>close</Icon>}
           header={<CardTitle image="/assets/project-images/employee-manager.png" reveal waves="light"/>}
-          reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}
-          revealIcon={<Icon>more_vert</Icon>}
-          title="CR-Shift Scheduler"
+          reveal={<><p>Employee-tracker is a CLI content management system that allows the user to manage employees. The app allows the user to add/remove employees, departments and roles. Roles, Managers and employees can be views edited accordingly.<br/><br/>
+            </p>
+            <div className="cardLinks">
+            
+              <a href="https://github.com/Mohmedvaid/employee-tracker" target="_blank">
+                Github
+              </a>
+            </div></>}          revealIcon={<Icon>more_vert</Icon>}
+          title="Employee Manager"
         >
           <p >
-            <a href="https://github.com/Mohmedvaid/note-taker">
+            <a href="https://github.com/Mohmedvaid/employee-tracker" target="_blank">
               Github
             </a>
           </p>
@@ -169,12 +224,20 @@ const Portfolio = () => (
          className="portfolio-cards"
           closeIcon={<Icon>close</Icon>}
           header={<CardTitle image="/assets/project-images/weather-app.png" reveal waves="light"/>}
-          reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}
-          revealIcon={<Icon>more_vert</Icon>}
-          title="CR-Shift Scheduler"
+          reveal={<><p>The Weather App shows basic weather data such as a 5-day forecast for a city based on user search! Boostrap and jQuery were used to build this app<br/><br/>
+            </p>
+            <div className="cardLinks">
+            <a href="https://mohmedvaid.github.io/weather-app/" target="_blank">
+                Deployed Site
+              </a><br/>
+              <a href="https://github.com/Mohmedvaid/weather-app" target="_blank">
+                Github
+              </a>
+            </div></>}          revealIcon={<Icon>more_vert</Icon>}
+          title="Weather App"
         >
           <p >
-            <a href="https://github.com/Mohmedvaid/weather-app">
+            <a href="https://github.com/Mohmedvaid/weather-app" target="_blank">
               Github
             </a>
           </p>
