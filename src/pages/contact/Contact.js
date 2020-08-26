b9a74c7b8e167dcef0b4996ac1f410e0a4631c76
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-08-25 23:22:14 -0500

    organized css files

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -19,7 +19,7 @@ const Contact = () => (
       <div className="social-media">
           <p className="form-label">Social Media</p>
           <div className="social-icons">
-              <a style={{color:"lightslategrey"}} href="/public/assets/Mohmedhusain_Vaid_Resume.docx"><i className="far fa-file fa-4x resume-icon" aria-hidden="true"></i></a>
+              <a style={{color:"lightslategrey"}} href="/assets/Mohmedhusain_Vaid_Resume.docx"><i className="far fa-file fa-4x resume-icon" aria-hidden="true"></i></a>
               <a style={{color:"lightslategrey"}} href="https://www.linkedin.com/in/mohmedhusain-v-0374b2128/"><i className="fab fa-linkedin fa-4x linkedin-icon" aria-hidden="true"></i></a>
               <a style={{color:"lightslategrey"}} href="https://github.com/Mohmedvaid/"><i className="fab fa-github-square fa-4x github-icon" aria-hidden="true"></i></a>
           </div>
